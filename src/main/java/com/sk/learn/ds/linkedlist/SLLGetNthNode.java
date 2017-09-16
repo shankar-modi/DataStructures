e10cb0c6ac64134bab49f673b2e8409a76a0f88f
Shankar <shankar.modi08@gmail.com>
2017-09-16 11:34:46 +0530

    Formatting

--- src/main/java/com/sk/learn/ds/linkedlist/SLLGetNthNode.java
+++ src/main/java/com/sk/learn/ds/linkedlist/SLLGetNthNode.java
@@ -7,23 +7,22 @@ package com.sk.learn.ds.linkedlist;
 public class SLLGetNthNode {
 
     static Node head;
+
     public static void main(String[] args) {
-         head = SLLUitility.createLinkedLIst(new int[]{1, 2, 3, 4, 5});
-
-        int node = getNthNode(4);
-
-        System.out.println("Nth node at index : " + node);
+        head = SLLUitility.createLinkedLIst(new int[]{1, 2, 3, 4, 5});
+        int index = 3;
+        System.out.println("Nth node at index : " + index + " is " + getNthNode(4));
     }
 
     private static int getNthNode(int index) {
-        if(index < 0) throw new RuntimeException("Index should be non negative number");
-        if(index > (SLLUitility.lenght(head) -1 ))throw new RuntimeException("IndexoutOfBound");
+        if (index < 0) throw new RuntimeException("Index should be non negative number");
+        if (index > (SLLUitility.lenght(head) - 1)) throw new RuntimeException("IndexoutOfBound");
 
         Node temp = head;
         int currentIndex = 0;
 
-        while (temp != null && currentIndex != index){
-            temp=temp.getNext();
+        while (temp != null && currentIndex != index) {
+            temp = temp.getNext();
             currentIndex++;
         }
 
